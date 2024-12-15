9a855addfc9e605b103753eb7e2c53896c3a1418
Ayush8795 <ayumanz8795@gmail.com>
2024-12-15 16:59:05 +0530

    updates

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, X, Rocket } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import logo from "../assets/images/Logo.jpeg";
 
 function Navbar() {
